f6f2bd1707410d307470d50e4801c06888046786
parkcrystal <9jeworld@naver.com>
2020-06-06 11:25:25 +0900

    프래그먼ㅌ

---
+++ app/src/main/java/com/phis/fragment_20200606/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.phis.fragment_20200606
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
---
+++ app/src/main/java/com/phis/fragment_20200606/fragments/FirstFragment.kt
@@ -0,0 +1,8 @@
+package com.phis.fragment_20200606.fragments
+
+import androidx.fragment.app.Fragment
+
+class FirstFragment : Fragment(){
+
+
+}
