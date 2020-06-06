a6a206b25c5dc2229c0da3ea2cee1635a18534aa
parkcrystal <9jeworld@naver.com>
2020-06-06 12:20:32 +0900

    SecondFragment 추가 => 스크롤뷰 대응

--- app/src/main/java/com/phis/fragment_20200606/fragments/SencondFragment.kt
+++ app/src/main/java/com/phis/fragment_20200606/fragments/SencondFragment.kt
@@ -6,25 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.phis.fragment_20200606.R
-import kotlinx.android.synthetic.main.fragment_first.*
+import kotlinx.android.synthetic.main.fragment_second.*
 
-class FirstFragment : Fragment(){
+class SencondFragment: Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        
         messageTxt.text = "안녕하세요"
     }
-
-
-
 
 }
